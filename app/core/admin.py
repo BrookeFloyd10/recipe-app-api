7a4763d9561cdc88d058517b9592665e8a5db314
Brooke Floyd <brooke.floyd10@gmail.com>
2025-10-09 16:04:07 -0500

    made changes from code checker

--- app/core/admin.py
+++ app/core/admin.py
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
         (
             _('Permissions'),
             {
-                'fields':(
+                'fields' :(
                     'is_active',
                     'is_staff',
                     'is_superuser',
@@ -38,7 +38,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
 
