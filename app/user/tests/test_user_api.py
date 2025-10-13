6c86791ff53d93a8b21d31915ce752b78a421e27
Brooke Floyd <brooke.floyd10@gmail.com>
2025-10-13 15:47:00 -0500

    test for password length

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,10 +44,12 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
+
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
         def test_password_too_short_error(self):
             """Test an error is returned if password less than 5 chars"""
             payload = {
@@ -57,7 +59,7 @@ class PublicUserApiTests(TestCase):
             }
             res = self.client.post(CREATE_USER_URL, payload)
 
-            self/asssertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
             user_exists = get_user_model().objects.filter(
                 email=payload['email']
             ).exists()
